49d0043b3de46f18bf5f520bb70f667e6fe0a982
saisameerd <saisameer.d@gmail.com>
2022-04-15 18:52:24 +0530

    Added arrow funcs

---
+++ Prerequisites/arrowFunction.js
@@ -0,0 +1,13 @@
+let regular =  function(name){
+    console.log(`I am ${name}`)
+}
+
+let arrowfunc = (name) =>{
+    console.log(`I am ${name}`)
+}
+
+let arrowshort = name => console.log(`I am  ${name}`)
+
+regular('varma');
+arrowfunc('vinay');
+arrowshort('neelish')
